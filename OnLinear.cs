6d8d264b3fb0b8fc2bfee72f306318a98a6e5464
Daniel Ontiveros <cardanont@gmail.com>
2019-06-25 16:36:42 -0500

    Add project files.

---
+++ OnLinear.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigOStuff
+{
+    class OnLinear
+    {
+        
+        public void FindNemo(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == "nemo")
+                    Console.WriteLine("Found Nemo!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+namespace BigOStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nemo = { "bruce", "dory", "marlin", "nigel", "squirt", "darla", "hank", "something"
+                            , "gill", "nemo", "whatever", "here"};
+
+            OnLinear Linear = new OnLinear();
+
+            Linear.FindNemo(nemo);
+        }
+    }
+}
