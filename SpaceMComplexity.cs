c71cebcdaa7330edf8af5ac6810fef056ed06231
Daniel Ontiveros <cardanont@gmail.com>
2019-06-26 15:39:47 -0500

    Memory Complexity

---
+++ SpaceMComplexity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigOStuff
+{
+    class SpaceMComplexity
+    {
+        // M.C = O(1)
+        public void Boooo(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Boooo");
+            }
+        }
+
+
+
+        // M.C = O(n)
+        public string arrayOfHiNtimes(int n)
+        {
+            string[] hiArray = { };
+            for (int i = 0; i < n; i++)
+            {
+                hiArray[i] = "hi";
+            }
+
+            return hiArray.ToString();
+        }
+    }
+}
